e232c36779f606109dd489ab50f30924aa04e8b1
henry blanchette <henry@monkeyface.com>
2019-06-04 10:47:46 -0400

    update compile

--- src/compile.sh
+++ src/compile.sh
@@ -10,16 +10,5 @@ ocamlfind ocamlopt \
   utility.mli    utility.ml    \
   wellformed.mli wellformed.ml
 
-# ocamlbuild -use-ocamlfind -pkgs ocaml-protoc -Is ast,util,rules,sat -libs str main.native
-# + ocamlfind ocamlc -c -annot -bin-annot -thread -package ocaml-protoc,core,ppx_jane,ppx_compare,ppx_sexp_conv,getopt,z3,ppx_let -package ocaml-protoc -I rules -I ast -I sat -I util -o rules/formula.cmo rules/formula.ml
-
-# ocamlbuild -use-ocamlfind -pkgs ocaml-protoc -libs str
-
-# echo "[*] building to main.native"
+echo "[*] building to main.native"
 ocamlbuild -use-ocamlfind -pkgs ocaml-protoc -libs str -no-hygiene main.native
-
-# ocamlfind ocamlopt -c
-#   -annot -bin-annot -thread \
-#   -package ocaml-protoc,core,ppx_jane,ppx_compare,ppx_sexp_conv,getopt,z3,ppx_let
-#   -o ast_pb.cmo
-#   ast_pb.ml
