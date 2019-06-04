ecba4fe819e0dc5362da4b975f704af651371721
henry blanchette <henry@monkeyface.com>
2019-06-04 11:02:35 -0400

    successful compil

--- src/compile.sh
+++ src/compile.sh
@@ -1,15 +1,20 @@
 echo "[*] compiling ast.proto"
 ocaml-protoc -pp -binary -ml_out ./ ast.proto
 
+# ocamlfind ocamlopt -c -thread -linkpkg -package core #
+
+echo "[*] compiling functools lib"
+ocamlopt -a -o functools.cmxa functools.mli functools.ml
+
 echo "[*] compiling .mli and .ml files"
 ocamlfind ocamlopt \
   -c -thread -linkpkg \
   -package core \
   ast_types.mli  ast_types.ml  \
-  functools.mli  functools.ml  \
   utility.mli    utility.ml    \
   wellformed.mli wellformed.ml
 
 echo "[*] building to main.native"
-ocamlbuild -use-ocamlfind -pkgs ocaml-protoc -libs str -no-hygiene main.native
-echo "[!] make sure to run 'export DYLD_LIBRARY_PATH=~/.opam/default/lib/z3/' to set path for the dynamic library `z3lib.dylib`"
+ocamlbuild -build-dir ./ -use-ocamlfind -pkgs ocaml-protoc -libs str,functools -no-hygiene main.native
+install main.native main
+echo "[!] make sure to run 'export DYLD_LIBRARY_PATH=~/.opam/default/lib/z3/' to set path for the dynamic library 'z3lib.dylib'"
